3f88d650e8a34ef2cd05c945f58fa5bdde2eba11
Anique <aniquetahir@gmail.com>
2017-10-02 01:46:38 -0700

    removed extra code added shebang

--- get_index_spatial_observation.sh
+++ get_index_spatial_observation.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Data Polygamy jar is stored in $DATA_POLYGAMY_JAR
 # Scripts are stored under $SCRIPTS_PATH
 # Data Polygamy is stored in $DATA_POLYGAMY
@@ -8,14 +9,12 @@ latindex=$2
 lngindex=$3
 zoneid=$4
 temporalindex=$5
-xindex=$6
-yindex=$7
 
 m="r3.2xlarge"
 n=1
 
 # Filter data based on zone
-hadoop fs -cat data/$dataname > /tmp/spatialdata 1>&2
+hadoop fs -cat data/$dataname > /tmp/spatialdata
 python3 $SCRIPTS_PATH/filternbhd.py $DATA_POLYGAMY/data/neighborhood.txt /tmp/spatialdata /tmp/spatialdata.filtered $latindex $lngindex $zoneid 1>&2
 
 # Store filtered data in hdfs
@@ -31,7 +30,7 @@ hadoop fs -cp data/$dataname.header data/filtereddata.header 1>&2
 # TODO Add filtered output to datasets
 
 # Preprocessing, Aggregation, Indexing
-hadoop jar $DATA_POLYGAMY_JAR edu.nyu.vida.data_polygamy.pre_processing.PreProcessing -m $m -n $n -dn filtereddata -dh filtereddata.header -dd filtereddata.defaults -t hour -s nbhd -cs points -i $5 $6 $7 1>&2
+hadoop jar $DATA_POLYGAMY_JAR edu.nyu.vida.data_polygamy.pre_processing.PreProcessing -m $m -n $n -dn filtereddata -dh filtereddata.header -dd filtereddata.defaults -t hour -s nbhd -cs points -i $temporalindex $lngindex $latindex 1>&2
 hadoop jar $DATA_POLYGAMY_JAR edu.nyu.vida.data_polygamy.scalar_function_computation.Aggregation -m $m -n $n -g filtereddata 0 0 1>&2
 hadoop jar $DATA_POLYGAMY_JAR edu.nyu.vida.data_polygamy.feature_identification.IndexCreation -f -m $m -n $n -g filtereddata 1>&2
 
