dd99898947cb55c370134c33c6926410bdec68af
Anique <aniquetahir@gmail.com>
2017-11-15 18:53:00 -0700

    Implemented popen

--- QualitativeEvaluation.py
+++ QualitativeEvaluation.py
@@ -1,24 +1,51 @@
 import calculate_salient_explanation
+import subprocess
+
+attributes = []
+processor_home = '/home/anique/IdeaProjects/yellowtaxi'
+data_file = "yellowdata_pickup.csv"
+
 
 class Aggravation:
     def __init__(self):
         pass
 
-    def judge(self, attribute, zones, bucket):
+    def judge(self, observation, attribute, zones, min, max):
         """
         :param attribute: the attribute in the explanation predicate
         :param zones: the zones in the explanation predicate
         :param buckets: the bucket for the explanation predicate
         :return: the degree of explanation by aggravation(intensity) for the provided input
         """
-        pass
+        # Create predicate
+        predicate = " or ".join(['zone=%i' % x for x in zones])
+        predicate = "(%s)" % predicate
+        if min:
+            predicate = "%s and %s>%f" % (predicate, attribute, min)
+
+        if max:
+            predicate = "%s and %s<%f" % (predicate, attribute, max)
+
+        # Create subprocess
+        query = subprocess.run([
+            'java',
+            '-cp',
+            '%s/target/yellowtaxi-1.0-SNAPSHOT-jar-with-dependencies.jar'%processor_home,
+            'edu.asu.query.Query',
+            "%s/data/%s" % (processor_home, data_file),
+            predicate,
+            observation
+        ], stdout=subprocess.PIPE)
+
+        # Return subprocess output
+        return float(query.stdout.decode('utf-8'))
 
 
 class Intervention:
     def __init__(self):
         pass
 
-    def judge(self, attribute, zones, bucket):
+    def judge(self, observation, attribute, zones, min, max):
         """
         :param attribute: the attribute in the explanation predicate
         :param zones: the zones in the explanation predicate
@@ -32,7 +59,8 @@ def evaluate_salient_features():
     # Evaluate tip percentage
 
     # Get Top 5 explanations
-    top_five = calculate_salient_explanation.top_explanations(5, 'index/data')
+    observation_att = 5
+    top_five = calculate_salient_explanation.top_explanations(observation_att, 'index/data')
     intensity = []
     influence = []
     for explanation in top_five:
@@ -41,10 +69,11 @@ def evaluate_salient_features():
         else:
             zones = map(lambda x: x['zone'], explanation['negative'])
 
-        attribute = explanation['attribute']
+        attribute = attributes[explanation['attribute']]
 
-        intensity.append(Aggravation().judge(attribute, zones, None))
-        influence.append(Intervention().judge(attribute, zones, None))
+        observation = "avg(%s)" % attributes[observation_att]
+        intensity.append(Aggravation().judge(observation, attribute, zones, None, None))
+        influence.append(Intervention().judge(observation, attribute, zones, None, None))
 
     # TODO Draw Graph
     pass
@@ -64,6 +93,14 @@ def evaluate_heirarchical_intervention():
 
 
 if __name__ == "__main__":
-    #
+    # Evaluate yellowcab data
+    # Load attributes
+    headers_filename = "yellowdata.header"
+    headers_file = open(headers_filename, 'r')
+    attributes = [x.strip() for x in headers_file.readline().split(',')]
+    headers_file.close()
 
-    pass
+    evaluate_salient_features()
+    evaluate_aggravation()
+    evaluate_intervention()
+
